95465f6b096fcae7a9fcf508675b78cd86d87540
Ayomiidey <quadriayomiidey@gmail.com>
2024-10-06 17:46:51 +0100

    my first nest js project

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 300 }]),
+  ],
+  controllers: [AppController],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
+})
+export class AppModule {}
